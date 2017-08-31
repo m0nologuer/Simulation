d65da019266051e333169bcd6e49f59671f78a26
Sakunthala Panditharatne <saku.p@cantab.net>
2017-08-31 01:50:46 +0100

    new thing

--- pingpong.py
+++ pingpong.py
@@ -14,13 +14,13 @@ class PingPong(Component):
 	def act(self, action, object_array):
 		#4 axes - yaw pitch roll rotation
 		# + x position
-		#self.model.parent.node().addTorque(LRotationf(action[0], action[1], action[2]))
-		#self.model.parent.node().addImpulse(Vec3(action[3], 0, 0))
+		self.model.parent.node().applyTorqueImpulse(LRotationf(action[0], action[1], action[2]))
+		self.model.parent.node().applyCentralImpulse(Vec3(action[3], 0, 0))
 
 
 	def update(self, task):
 
-		#correction = (self.model.parent.getPos() - Vec3(0,5,5)) * 0.01
-		#self.model.parent.node().addImpulse(correction)
+		correction = (self.model.parent.getPos() - Vec3(0,5,5)) * 0.01
+		self.model.parent.node().applyCentralImpulse(correction)
 
 		return task.cont
